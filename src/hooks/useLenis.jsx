65353cfb78084f5a1051b752e5c1560ea8f04506
J Carlos Rojas <113724270+J-CarlosRojas@users.noreply.github.com>
2025-07-15 11:25:58 -0500

    instalacion de Lenis

---
+++ src/hooks/useLenis.jsx
@@ -0,0 +1,22 @@
+// hooks/useLenis.js
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
+
+export default function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smooth: true,
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+}
